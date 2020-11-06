739d6a448af70d1ff9f7264d907f5ef6da3c276d
Luna <Kobali17@yandex.ru>
2020-11-06 22:56:42 +0300

    app.get fix

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 
+app.use(express.static(path.join(__dirname, 'public/build')));
+app.use('/', cards);
+app.use('/', users);
 app.get('*', (req, res, next) => {
   res.status(404);
   res.send({ message: 'Запрашиваемый ресурс не найден' });
   next();
 });
-app.use(express.static(path.join(__dirname, 'public/build')));
-app.use('/', cards);
-app.use('/', users);
 
 app.listen(PORT, () => {
   console.log(`Listen on port ${PORT}`);
