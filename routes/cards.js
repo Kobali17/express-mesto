d99baa119a86d1dee2ad4b828df44760d426f49c
Luna <Kobali17@yandex.ru>
2020-11-09 00:10:37 +0300

    fix users

--- routes/cards.js
+++ routes/cards.js
@@ -3,9 +3,8 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-const cards = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/cards.json'), 'utf8'));
-
 router.get('/cards', (req, res) => {
+  const cards = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/cards.json'), 'utf8'));
   res.send(cards);
 });
 
