303a1af47ea16f22ae432c4679a2121c41f6655c
Luna <Kobali17@yandex.ru>
2020-11-09 14:15:45 +0300

    fix users

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,27 @@
 const router = require('express').Router();
 
 const path = require('path');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
-const sendUser = (req, res) => {
-  const users = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf8'));
+const sendUser = (req, res, next) => fsPromises.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' }).then((data) => {
+  const users = JSON.parse(data);
   const thisUsers = users.filter((user) => user._id === req.params.id);
   if (thisUsers.length === 0) {
     res.status(404);
     res.send({ message: 'Нет пользователя с таким id' });
   } else {
-    res.send(users[req.params.id]);
+    res.send(thisUsers[0]);
   }
-};
-
-router.get('/users', (req, res) => {
-  const users = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf8'));
-  res.send(users);
+}).catch((err) => {
+  next(err);
 });
+
+router.get('/users', (req, res, next) => fsPromises.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' }).then((data) => {
+  const users = JSON.parse(data);
+  res.send(users);
+}).catch((err) => {
+  next(err);
+}));
 
 router.get('/users/:id', sendUser);
 
